fb2d02b224b40d498512ebf95f0f502d39443f97
Abe Vriens <abe.vriens@gmail.com>
2020-12-03 00:09:02 +0100

    Woorden toevoegen functionaliteit

--- index.php
+++ index.php
@@ -6,10 +6,15 @@ include 'header.php';
         <main>
         <div class="wrapper">
             <form id="woorden">
-                <div id="woordendiv">
-                    <input class="woord" id="woord1" placeholder="Woord" type="text" value="">
-                    <input class="woord" id="woord2" placeholder="Woord vertaling" type="text" value="">
+                <div id="woordentotaal">
+                    <div id="woordendiv">
+                        <input class="woord" id="woord1" placeholder="Woord" type="text" value="">
+                        <input class="woord" id="woord2" placeholder="Woord vertaling" type="text" value="">
+                    </div>
                 </div>
+                <input type="number" id="hoeveelheid" name="hoeveelheid" min="1" max="100">
+                <input type="button" value="Voeg 5 woorden toe" onclick="woordenToevoegen()">
+                <input type="submit" value="Klaar">
             </form>
         </div>
         </main>
--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 let allInputs = Array();
 let currentInput;
 
-document.addEventListener("keydown", function (e) {
+$(document).on("keydown", "input", function (e) {
     if(e.key != "Tab") return;
     allInputs = Array.from(document.getElementsByClassName("woord"));
     currentInput = allInputs[allInputs.indexOf(document.activeElement) + 1];
-    let nextInput = allInputs[allInputs.indexOf(document.activeElement) + 2];
-    let lastInput = allInputs[allInputs.length - 1];
 
     if(currentInput == null) {
         e.preventDefault();
-        createVraagDiv();
+        woordenToevoegen(1);
     }
 });
 
-const form = document.getElementById("woorden");
+let totaalWoordenDiv = document.getElementById("woordentotaal");
 
-function createVraagDiv() {
-    const div = document.getElementById("woordendiv").cloneNode();
-    let input1 = document.getElementById("woord1").cloneNode();
-    input1.value = "";
-    let input2 = document.getElementById("woord2").cloneNode();
-    input2.value = "";
-
-    form.appendChild(div);
-    div.appendChild(input1);
-    div.appendChild(input2);
-
-    input1.focus();
+function woordenToevoegen(hoeveelheid) {
+    if(hoeveelheid == null) hoeveelheid = document.getElementById("hoeveelheid").value;
+    for(let i = 1; i <= hoeveelheid; i++) {
+        let woordenDiv = document.createElement("div");
+        woordenDiv.id = "woordendiv";
+        let input1 = document.getElementById("woord1").cloneNode();
+        input1.value = "";
+        let input2 = document.getElementById("woord2").cloneNode();
+        input2.value = "";
+        totaalWoordenDiv.appendChild(woordenDiv);
+        woordenDiv.appendChild(input1);
+        woordenDiv.appendChild(input2);
+        input1.focus();
+    }
 }
