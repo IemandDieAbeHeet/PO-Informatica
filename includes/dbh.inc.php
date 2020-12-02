5b54691dbff84a988b7713b97792ae25ec690e90
Abe Vriens <abe.vriens@gmail.com>
2020-12-03 00:07:56 +0100

    Database verbinding toegevoegd

---
+++ includes/dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$servername = "localhost";
+$dBUsername = "root";
+$dBPassword = file_get_contents("E:/XAMPP/htdocs/has.txt") . "!";
+$dBName = "poinformatica";
+
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+
+if(!$conn) {
+    die("Connection failed: ".mysqli_connect_error());
+}
