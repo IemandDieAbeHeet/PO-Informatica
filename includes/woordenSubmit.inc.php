24c2f8b4720e94deab8ebd6fdb16ff6858bfb38a
Abe Vriens <abe.vriens@gmail.com>
2020-12-05 01:13:14 +0100

    Woordenlijst naar database

--- includes/woordenSubmit.inc.php
+++ includes/woordenSubmit.inc.php
@@ -1,12 +1,47 @@
 <?php
 
-if(isset($_POST["woordjes-submit"])) {
-    require 'dbh.inc.php';
+session_start();
 
-    $userId = $_SESSION["userId"];
-    $woordenLijstId = $_POST["woordenlijstId"];
-    $woordenLijst1 = $_POST["woorden1"];
-    $woordenLijst2 = $_POST["woorden2"];
+require 'dbh.inc.php';
+
+$userId = $_SESSION["userId"];
+$woordenlijstNaam = $_POST['title'];
+
+$woordenLijst1 = "";
+
+foreach($_POST["woordenArray"][0] as $woord) {
+    $woord_clean = str_replace(",", "", $woord);
+    $woordenLijst1 .= $woord_clean;
+}
+
+$woordenLijst2 = "";
+
+foreach($_POST["woordenArray"][1] as $woord) {
+    $woord_clean = str_replace(",", "", $woord);
+    $woordenLijst2 .= $woord_clean;
+}
+
+echo $woordenLijst1[0];
+echo $woordenLijst2[0];
+
+if(empty($userId)) {
+    header('HTTP/1.1 400 Je bent niet ingelogd!');
+} else if(empty($woordenlijstNaam) || empty($_POST["woordenArray"][0]) || empty($_POST["woordenArray"][1])) {
+    header('HTTP/1.1 400 Velden niet ingevuld!');
 } else {
-    header('HTTP/1.1 419 Form not submitted');
+    $sql = "INSERT INTO woordjes (userId, woordOrigineel, woordVertaling) VALUES (?, ?, ?)";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sql)) {
+        header('HTTP/1.1 400 SQL error!');
+        exit();
+    }
+    else {
+        mysqli_stmt_bind_param($stmt, "sss", $userId, $woordenLijst1, $woordenLijst2);
+        $result = mysqli_stmt_execute($stmt);
+
+        $affectedRow = mysqli_info($conn);
+
+        header('HTTP/1.1 200 Woordenlijst succesvol opgeslagen!');
+        exit();
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,15 +1,42 @@
-let allInputs = Array();
-let currentInput;
+$(window).on("load", function() {
+    checkForEmptyInputs();
+})
+
+$("#woordenForm input[type='text']").on("keyup", function() {
+    checkForEmptyInputs();
+});
+
+function checkForEmptyInputs() {
+    let anyEmpty = false;
+    let emptyAmount = 0;
+    $("#woordenForm input[type='text']").each(function() {
+        if(this.value.trim() == "") {
+            anyEmpty = true;
+        }
+    });
+
+    if(anyEmpty) {
+        $("#woordenSubmit").prop("disabled", true);
+    } else {
+        $("#woordenSubmit").prop("disabled", false);
+    }
+}
 
 $(document).on("keydown", ".woord", function (e) {
     if(e.key != "Tab") return;
-    allInputs = Array.from(document.getElementsByClassName("woord"));
-    currentInput = allInputs[allInputs.indexOf(document.activeElement) + 1];
+    let allInputs = Array.from(document.getElementsByClassName("woord"));
+    let currentInput = allInputs[allInputs.indexOf(document.activeElement) + 1];
 
     if(currentInput == null) {
         e.preventDefault();
         woordenToevoegen(1);
     }
+});
+
+$(document).on("click", "#verwijderWoord", function() {
+    let buttonParent = $(this).parent();
+    buttonParent.empty();
+    buttonParent.remove();
 });
 
 let totaalWoordenDiv = document.getElementById("woordentotaal");
@@ -18,14 +45,29 @@ function woordenToevoegen(hoeveelheid) {
     if(hoeveelheid == null) hoeveelheid = document.getElementById("hoeveelheid").value;
     for(let i = 1; i <= hoeveelheid; i++) {
         let woordenDiv = document.createElement("div");
-        woordenDiv.id = "woordendiv";
+        woordenDiv.className = "woordendiv";
+        totaalWoordenDiv.appendChild(woordenDiv);
+        
+        let vorigNummer = parseInt($("p#woordnummer").last().text());
+        let woordNummerElement = document.createElement("p");
+        woordNummerElement.id = "woordnummer";
+        let woordNummerNode = document.createTextNode(vorigNummer + 1);
+        woordNummerElement.appendChild(woordNummerNode);
+        woordenDiv.appendChild(woordNummerElement);
+        
         let input1 = document.getElementById("woord1").cloneNode();
         input1.value = "";
         let input2 = document.getElementById("woord2").cloneNode();
         input2.value = "";
-        totaalWoordenDiv.appendChild(woordenDiv);
         woordenDiv.appendChild(input1);
         woordenDiv.appendChild(input2);
+
+        let removeButton = document.createElement("input");
+        removeButton.type = "button";
+        removeButton.value = "X";
+        removeButton.id = "verwijderWoord"
+        woordenDiv.appendChild(removeButton);
+
         input1.focus();
     }
 }
@@ -42,15 +84,19 @@ $('#woordenForm').on('submit', function (e) {
     }
 
     $('#woordenForm #woord1').each(function(index){
-        let woord = {};
-        woord[index] = $(this).val();
-        woordenLijst.woordenArray[0].push($(this).val());
+        if(this.value.trim() != "") {
+            let woord = {};
+            woord[index] = $(this).val();
+            woordenLijst.woordenArray[0].push($(this).val());
+        }
     });
 
     $('#woordenForm #woord2').each(function(index){
-        let woord = {};
-        woord[index] = $(this).val();
-        woordenLijst.woordenArray[1].push($(this).val());
+        if(this.value.trim() != "") {
+            let woord = {};
+            woord[index] = $(this).val();
+            woordenLijst.woordenArray[1].push($(this).val());
+        }
     });
 
     console.log(woordenLijst);
@@ -59,14 +105,13 @@ $('#woordenForm').on('submit', function (e) {
       type: 'post',
       url: 'includes/woordenSubmit.inc.php',
       data: woordenLijst,
-      success: function (data) {
-        if(!data.success) {
-            alert(data.error);
-            $("#error").val(data.error);
-        }
+      success: function (xhr) {
+        $("#response").attr("class", "success");
+        $("#response").text(xhr.statusText)
       },
       error: function(xhr) {
-          alert(xhr.status);
+        $("#response").attr("class", "error");
+        $("#response").text(xhr.statusText);
       }
     });
 });
