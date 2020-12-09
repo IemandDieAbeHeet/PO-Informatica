983220dd88931a03228ff14f5ba43de4070d7cd4
IemandDieAbeHeet <abe.vriens@gmail.com>
2020-12-10 00:16:48 +0100

    Naam update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['userId'])) {
 <main class="main">
     <header>
         <div>
-            <h1>WRTS 2.0</h1>
+            <h1>Wörterwelt</h1>
         </div>
         <nav>
             <a href="login">Inloggen</a>
