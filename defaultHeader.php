e5b4e513055ba0c8db63a9b61375c80cde5edca9
IemandDieAbeHeet <abe.vriens@gmail.com>
2020-12-22 20:19:19 -0500

    Variabelen verkeerde naam updaten

--- defaultHeader.php
+++ defaultHeader.php
@@ -8,7 +8,7 @@ session_start();
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="description" content="WRTS maar dan anders">
+        <meta name="description" content="Woordjes leren, maar dan op een leuke manier.">
         <title>Wörterwelt</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/style.css">
