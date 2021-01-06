119e21ea35bbf343dae8cec9f8086afa9c293433
Abe Vriens <abe.vriens@gmail.com>
2021-01-06 12:51:57 +0100

    Database bestanden update

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$dBUsername = file_get_contents("../username.txt");
-$dBPassword = file_get_contents("../wachtwoord.txt");
-$dBName = file_get_contents("../database.txt");
+$dBUsername = file_get_contents("username.txt", true);
+$dBPassword = file_get_contents("wachtwoord.txt", true);
+$dBName = file_get_contents("database.txt", true);
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
