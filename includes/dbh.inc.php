5c4c364a25d752548f5c2cbbd3d489040e96344a
¨Lieve <lieveverhoeven12@gmail.com>
2021-01-06 12:57:19 +0100

    Update

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 //
 $servername = "localhost";
-$dBUsername = file_get_contents("username.txt", true);
-$dBPassword = file_get_contents("wachtwoord.txt", true);
-$dBName = file_get_contents("database.txt", true);
+$dBUsername = file_get_contents("../username.txt", true);
+$dBPassword = file_get_contents("../wachtwoord.txt", true);
+$dBName = file_get_contents("../database.txt", true);
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
