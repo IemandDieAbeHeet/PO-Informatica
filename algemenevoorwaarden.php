05d15292382161dafca0fadc795e7878cc59a11d
Abe Vriens <abe.vriens@gmail.com>
2021-01-09 23:22:21 +0100

    Algemene voorwaarden toevoegen

---
+++ algemenevoorwaarden.php
@@ -0,0 +1,50 @@
+<?php
+    require "defaultHeader.php";
+    require "loginDisallowed.php";
+?>
+<main>
+    <p id="algemenevoorwaarden">
+    Algemene voorwaarden voor WörterWelt.com
+    Welkom bij de algemene voorwaarden van 'www.WörterWelt.com’. Hier lees je alle afspraken die je met 'groep 2’ maakt bij gebruik van ons platform. Lees deze voorwaarden goed door, om misverstanden te voorkomen. Als er iets aan onze voorwaarden veranderd dan krijgt u dit te zien op onze site of per e-mail.
+    
+    Wij zijn gevestigd aan Noordhoekring 99, 5038 GC, Tilburg. Je kan contact opnemen door een e-mail te sturen naar onze klantenservice via WörterWelt@gmail.com.
+    
+    Definities:
+    Klant: De gebruiker van de site 'www.WörterWelt.com’.
+    Producent:  'groep 2’, het bedrijf dat verantwoordelijk is voor de site.
+    Product: 'www.WörterWelt.com’
+    
+    Toegang tot 'www.WörterWelt.com’:
+    Om een account aan te kunnen maken moet u eerst akkoord gaan met onze algemene voorwaarden. Vul al uw gegevens eerlijk in en houdt uw gegevens geheim. Je bent namelijk zelf verantwoordelijk voor alles wat er via jouw account gebeurt.
+
+    Om gebruik te maken van 'www.WörterWelt.com’ moet je overeenkomen met onze algemene voorwaarden, zo kan je een account aanmaken en inloggen op onze site. 
+    Je moet je inloggegevens geheim houden, want alles wat er op je account gebeurt is je eigen verantwoordelijkheid en 'groep 2’ is vrijgesteld van alle aansprakelijkheden van derden als het aantoonbaar is dat het je eigen fout is.
+    Het is aantoonbaar jouw fout wanneer:
+    Je via iemand anders account inlogt.
+    Je eigen gegevens lekt of aan iemand anders geeft.
+    Alle gegevens die je invult moeten geldig en compleet zijn, als dit niet het geval is moet je dit onmiddellijk aanpassen. 
+    'Groep 2’ garandeert niet dat er geen storingen kunnen zijn, en wanneer dit opgelost wordt is de site niet altijd bereikbaar. 'Groep 2’ is niet verantwoordelijk voor de eventuele schade door het tijdelijk onbeschikbaar zijn van 'www.WörterWelt.com’.
+
+    Gebruiken van 'www.WörterWelt.com’:
+    'www.WörterWelt.com’ is een site waar iedereen veilig en prettig moet kunnen werken. Dit betekent dat er geen dingen stuk gemaakt mogen worden (hacken of virussen plaatsen), geld verdiend mag worden (commercieel doeleinde) of onbehoorlijk taalgebruik (schelden en pesten). Zodra 'groep 2’ hier lucht van krijgt wordt je toegang verhinderd en mag je geen gebruik meer maken van de site. Bij eventuele schade met kosten word je hiervoor aansprakelijk gesteld.
+
+
+
+    Intellectueel eigendom:
+    Het gebruik van de informatie op 'www.WörterWelt.com’ is gratis zolang u deze informatie niet kopieert, verspreidt of op een andere manier gebruikt of misbruikt. U mag de informatie op deze website alleen hergebruiken volgens de regelingen van het dwingend recht.
+    Zonder uitdrukkelijke schriftelijke toestemming van is het niet toegestaan tekst, fotomateriaal of andere materialen op deze website her te gebruiken. Het intellectueel eigendom berust bij 'groep 2’.
+
+    Garantie:
+    'Groep 2’ geeft nergens garantie voor. Wij kunnen niet garanderen dat je schoolresultaten beter worden door het gebruiken van 'www.WörterWelt.com’. 'www.WörterWelt.com’ is er alleen voor bedoeld om te ondersteunen in het leren.
+
+    Aansprakelijkheid:
+    'Groep 2’ is nergens direct aansprakelijk voor. Als er ergens een probleem is proberen wij dit zo goed mogelijk op te lossen.
+
+    Privacy: 
+    Alle gegevens die je ons verstrekt blijven geheim en worden niet gedeeld met derden.
+    </p>
+</main>
+
+<?php
+    require "defaultFooter.php";
+?>
