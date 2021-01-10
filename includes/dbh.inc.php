5510747af8b3f45cde035968ed15e7c26d565826
Abe Vriens <abe.vriens@gmail.com>
2021-01-10 01:06:32 +0100

    Database inlog update

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 //
 $servername = "localhost";
-$dBUsername = file_get_contents("../username.txt", true);
-$dBPassword = file_get_contents("../wachtwoord.txt", true);
-$dBName = file_get_contents("../database.txt", true);
+$dBUsername = file_get_contents(__DIR__ . "/../username.txt");
+$dBPassword = file_get_contents(__DIR__ . "/../wachtwoord.txt");
+$dBName = file_get_contents(__DIR__ . "/../database.txt");
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
