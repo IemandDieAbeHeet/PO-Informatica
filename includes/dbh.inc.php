78039fe31273277fad053507dc42b4fd3314ea2a
Abe Vriens <abe.vriens@gmail.com>
2021-03-09 11:19:37 +0100

    Verwijder newlines van login bestanden

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,11 @@
 <?php
-//
-define("DB_SERVER", "localhost");
-define("DB_USER", file_get_contents(__DIR__ . "/../username.txt"));
-define("DB_PASSWORD", file_get_contents(__DIR__ . "/../wachtwoord.txt"));
-define("DB_DATABASE", file_get_contents(__DIR__ . "/../database.txt"));
 
-$conn = mysqli_connect(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);
+$db_server = "localhost";
+$db_user = preg_replace("/\s+/", "", file_get_contents(__DIR__ . "/../username.txt"));
+$db_pass = preg_replace("/\s+/", "", file_get_contents(__DIR__ . "/../wachtwoord.txt"));
+$db_database = preg_replace("/\s+/", "", file_get_contents(__DIR__ . "/../database.txt"));
+
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_database);
 
 if(!$conn) {
     die("Connection failed: ".mysqli_connect_error());
