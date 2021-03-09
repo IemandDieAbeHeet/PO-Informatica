3b4c69a1393fa976371e5dcb63c3208093e94d98
Abe Vriens <abe.vriens@gmail.com>
2021-03-09 10:25:34 +0100

    Database login fix

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,11 @@
 <?php
 //
-$servername = "localhost";
-$dBUsername = file_get_contents(__DIR__ . "/../username.txt");
-$dBPassword = file_get_contents(__DIR__ . "/../wachtwoord.txt");
-$dBName = file_get_contents(__DIR__ . "/../database.txt");
+define("DB_SERVER", "localhost");
+define("DB_USER", file_get_contents(__DIR__ + "../username.txt"));
+define("DB_PASSWORD", file_get_contents(__DIR__ + "../wachtwoord.txt"));
+define("DB_DATABASE", file_get_contents(__DIR__ + "../database.txt"));
 
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+$conn = mysqli_connect(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);
 
 if(!$conn) {
     die("Connection failed: ".mysqli_connect_error());
