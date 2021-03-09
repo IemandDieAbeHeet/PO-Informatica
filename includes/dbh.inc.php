f0ecc8ccf5d112b621a43003e46e1620ebf4863e
Abe Vriens <abe.vriens@gmail.com>
2021-03-09 10:31:02 +0100

    Database login files directory update

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 //
 define("DB_SERVER", "localhost");
-define("DB_USER", file_get_contents(__DIR__ + "../username.txt"));
-define("DB_PASSWORD", file_get_contents(__DIR__ + "../wachtwoord.txt"));
-define("DB_DATABASE", file_get_contents(__DIR__ + "../database.txt"));
+define("DB_USER", file_get_contents(__DIR__ . "/../username.txt"));
+define("DB_PASSWORD", file_get_contents(__DIR__ . "/../wachtwoord.txt"));
+define("DB_DATABASE", file_get_contents(__DIR__ . "/../database.txt"));
 
 $conn = mysqli_connect(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);
 
