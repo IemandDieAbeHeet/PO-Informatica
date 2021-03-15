297041b147840990945a29619a88c325727a8a79
IemandDieAbeHeet <abe.vriens@gmail.com>
2021-03-15 23:21:33 +0100

    Galgje toevoegen

---
+++ galgje.php
@@ -0,0 +1,23 @@
+<div>
+    <img id="balk_onder">
+    <img id="balk_links">
+    <img id="balk_boven">
+    <img id="touw">
+    
+    <img id="hoofd">
+    <img id="nek">
+    <img id="buik">
+    <img id="been_1">
+    <img id="been_2">
+    <img id="arm_1">
+    <img id="arm_2">
+    <img id="gezicht">
+
+    <input type="text" maxlength="8" id="galgjeInput">
+</div>
+
+<div id="resultaatDiv">
+    <p id="cijferText">Cijfer: </p>
+    <button id="resultaatButton">Terug naar lijsten</button>
+    <button id="foutenImporterenButton">Nieuwe lijst van fouten</button>
+</div>
