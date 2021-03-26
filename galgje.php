fea541aa4fc68b6309a373a753b7b9a40eae700f
Abe Vriens <abe.vriens@gmail.com>
2021-03-26 10:40:11 +0100

    Galgje toevoegen

---
+++ galgje.php
@@ -0,0 +1,17 @@
+<p class="oefenWoord">Woord</p>
+<input type="text" id="oefenInput">
+<button id="oefenButton">Nakijken</button>
+
+<div id="galgDiv">
+    <img id="basis">
+    <img id="steunpaal">
+    <img id="bovenbalk">
+    <img id="touwtje">
+    <img id="hoofd">
+    <img id="lichaam">
+    <img id="been1">
+    <img id="been2">
+    <img id="arm1">
+    <img id="arm2">
+    <img id="gezicht">
+</div>
