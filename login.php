c00a976bf0e843ab2122b9a93a22d999973c79d2
IemandDieAbeHeet <abe.vriens@gmail.com>
2021-04-07 17:50:44 +0200

    Inlog pagina CSS update

--- login.php
+++ login.php
@@ -5,25 +5,32 @@
 
     <main>
         <div class="wrapper">
-            <h1>Inloggen</h1>
-            <div class="login">
-            <?php
-            if(isset($_GET["error"])) {
-                if($_GET["error"] == "emptyfields") {
-                    echo '<p>Vul alle velden in!</p>';
-                }
-            }
-            else if(isset($_GET["login"])) {
-                if($_GET["login"] == "success") {
-                    echo '<p>Login gelukt!</p>';
-                }
-            }
-            ?>
-            <form action="includes/login.inc.php" method="post">
-                <input type="text" name="mailuid" placeholder="Gebruikersnaam/Email">
-                <input type="password" name="pwd" placeholder="Wachtwoord">
-                <button type="submit" name="login-submit">Inloggen</button>
-            </form>
+            <div id="loginMenu">
+                <h1 id="welcomeMessage">Welkom!</h1>
+                <div class="outerBox">
+                    <div class="box">
+                        <img id="welkomLogo" src="img/Logo.png">
+                    </div>
+                    <div class="box">
+                        <?php
+                        if(isset($_GET["error"])) {
+                            if($_GET["error"] == "emptyfields") {
+                                echo '<p>Vul alle velden in!</p>';
+                            }
+                        }
+                        else if(isset($_GET["login"])) {
+                            if($_GET["login"] == "success") {
+                                echo '<p>Login gelukt!</p>';
+                            }
+                        }
+                        ?>
+                        <form action="includes/login.inc.php" method="post" id="loginForm">
+                            <input type="text" name="mailuid" placeholder="Gebruikersnaam/Email">
+                            <input type="password" name="pwd" placeholder="Wachtwoord">
+                            <button type="submit" name="login-submit">Inloggen</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
