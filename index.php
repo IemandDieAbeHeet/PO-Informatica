aa72ca2ef23520fb742b83a9aaee0430be3cc301
Abe Vriens <abe.vriens@gmail.com>
2021-04-08 20:52:05 +0200

    Change index font size

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require "loginDisallowed.php";
     <header class="indexHeader">
         <div id="logoDiv">
             <img id="indexLogo" src="img/Logo.png">
-            <div><h1 style="color: orange">Wörter</h1><h1 style="color: #ccc">welt</h1></div>
+            <div><h1 style="color: orange">Wörter</h1><h1 style="color: white">welt</h1></div>
         </div>
         <nav class="indexNav">
             <button onclick="window.location.href='login.php'">Inloggen</button>
